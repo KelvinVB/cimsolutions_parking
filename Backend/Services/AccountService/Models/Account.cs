8edb5d96c518884b920e01a4d7fb113baf7fdf36
Boekel,Kelvin K. van <k.vanboekel@student.fontys.nl>
2021-03-29 17:04:12 +0200

    Setup account

---
+++ Backend/Services/AccountService/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountService.Models
+{
+    public class Account
+    {
+        public string AccountID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public DateTime dateOfBirth { get; set; }
+    }
+}
---
+++ Backend/Services/AccountService/Models/Authentication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountService.Models
+{
+    public class Authentication
+    {
+        public string AccountID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
