f98c6b1da574e656f93ea940470b203fb6acece3
Boekel,Kelvin K. van <k.vanboekel@student.fontys.nl>
2021-04-26 16:58:17 +0200

    models

---
+++ Frontend/Frontend/MobileApp/MobileApp/Models/ParkingGarage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileApp.Models
+{
+    public class ParkingGarage
+    {
+        public int parkingGarageID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string postcode { get; set; }
+        public int totalParkingSpots { get; set; }
+        public int freeParkingSpots { get; set; }
+        public List<ParkingSpot> parkingSpots { get; set; }
+    }
+}
