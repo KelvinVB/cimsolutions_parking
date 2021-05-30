2a5cc94cb209da10784a86f9902bfaa446b49205
Boekel,Kelvin K. van <k.vanboekel@student.fontys.nl>
2021-05-30 23:29:13 +0200

    update ui reservation page

--- Frontend/Frontend/MobileApp/MobileApp/Views/ReservationPage.xaml.cs
+++ Frontend/Frontend/MobileApp/MobileApp/Views/ReservationPage.xaml.cs
@@ -16,6 +16,7 @@ namespace MobileApp.Views
     {
         ParkingSpotViewModel parkingSpotViewModel;
         AccountViewModel accountViewModel;
+
         public ReservationPage(ParkingSpotViewModel viewModel, AccountViewModel accountViewModel)
         {
             InitializeComponent();
@@ -38,8 +39,16 @@ namespace MobileApp.Views
             timeSlot.endReservation = DatePickerEnd.Date + TimePickerEnd.Time;
             timeSlot.licensePlateNumber = labelLicensePlate.Text;
 
+            if (timeSlot.endReservation <= timeSlot.startReservation)
+            {
+                await DisplayAlert("Error", "Invalid time slot, please fill in the correct values", "Ok");
+                return;
+            }
+
             try
             {
+                //confirmation
+
                 timeSlot = await parkingSpotViewModel.ReserveWithAccount(timeSlot);
                 await DisplayAlert("Success", "Reservation planned on: " + timeSlot.startReservation.ToString() + " untill: " + timeSlot.endReservation.ToString(), "Ok");
 
@@ -60,19 +69,72 @@ namespace MobileApp.Views
 
         public async void OnButtonChangeClicked(object sender, EventArgs args)
         {
-            string result = await DisplayPromptAsync("License plate number", "Please fill in your license plate number.", maxLength:8, keyboard:Keyboard.Create(KeyboardFlags.CapitalizeCharacter));
-            if(result == null)
+            string result = await DisplayPromptAsync("License plate number", "Please fill in your license plate number.", maxLength: 8, keyboard: Keyboard.Create(KeyboardFlags.CapitalizeCharacter));
+            if (result == null)
             {
                 return;
             }
 
-            if(result.Length < 6)
+            if (result.Length < 6)
             {
                 await DisplayAlert("Error", "Please fill in a correct license plate number", "Ok");
                 return;
             }
             result.ToUpper();
             labelLicensePlate.Text = result;
+        }
+
+        private void DateSelectedEvent(object sender, EventArgs args)
+        {
+            DateTime startDate = DatePickerStart.Date + TimePickerStart.Time;
+            DateTime endDate = DatePickerEnd.Date + TimePickerEnd.Time;
+
+            TimeSpan span = (endDate - startDate);
+
+            int days = span.Days;
+            int hours = span.Hours;
+            int minutes = span.Minutes;
+            hours = hours + (days * 24);
+
+            if (hours >= 0 && minutes >= 0)
+            {
+                if (hours == 1)
+                {
+                    EntryDurationHours.Text = hours.ToString();
+                    LabelHours.Text = "hour";
+                }
+                else
+                {
+                    EntryDurationHours.Text = hours.ToString();
+                    LabelHours.Text = "hours";
+                }
+                if (minutes == 1)
+                {
+                    EntryDurationMinutes.Text = minutes.ToString();
+                    LabelMinutes.Text = "minute";
+                }
+                else
+                {
+                    EntryDurationMinutes.Text = minutes.ToString();
+                    LabelMinutes.Text = "minutes";
+                }
+            }
+
+        }
+
+        private void EntryTimeEvent(object sender, EventArgs args)
+        {
+            DateTime startDate = DatePickerStart.Date + TimePickerStart.Time;
+
+            int hours = Int32.Parse(EntryDurationHours.Text);
+            int minutes = Int32.Parse(EntryDurationMinutes.Text);
+
+            DateTime endDate = startDate;
+            endDate = endDate.AddHours(hours);
+            endDate = endDate.AddMinutes(minutes);
+
+            DatePickerEnd.Date = endDate.Date;
+            TimePickerEnd.Time = new TimeSpan(endDate.Hour, endDate.Minute, 0);
         }
     }
 }
