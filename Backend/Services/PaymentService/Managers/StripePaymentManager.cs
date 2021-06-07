96c6eacfe8603b49e270838ce53d88691fa946f3
Boekel,Kelvin K. van <k.vanboekel@student.fontys.nl>
2021-06-08 00:28:26 +0200

    get customer payment

--- Backend/Services/PaymentService/Managers/StripePaymentManager.cs
+++ Backend/Services/PaymentService/Managers/StripePaymentManager.cs
@@ -14,11 +14,18 @@ namespace PaymentService.Managers
             throw new NotImplementedException();
         }
 
-        public async Task<dynamic> PayByCreditCard(string cardnumber, int month, int year, string cvc, int value)
+        public async Task<dynamic> PayByCreditCard(string id, string email, string firstName, string lastName, string cardnumber, int month, int year, string cvc, int value)
         {
             try
             {
                 StripeConfiguration.ApiKey = "sk_test_51Iyd0JCW5oBVi3aeyirlYffw09mn2TFbGyt10imL1VdyHYJq46wYgBs4fF6xMLhZBhGqkAwfQrJ9PpQ6qxT8XBZT000gUFLyzy";
+
+                Customer customer = await GetCustomer(id);
+
+                if (customer == null)
+                {
+                    customer = await PostCustomer(id, email, firstName, lastName);
+                }
 
                 TokenCreateOptions optionstoken = new TokenCreateOptions
                 {
@@ -39,13 +46,66 @@ namespace PaymentService.Managers
                     Amount = value,
                     Currency = "eur",
                     Description = "testing stripe payment",
-                    Source = token.Id
+                    Source = token.Id,
+                    Customer
                 };
 
                 ChargeService service = new ChargeService();
                 Charge charge = await service.CreateAsync(options);
 
                 return charge.Paid;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<dynamic> GetCustomer(string id)
+        {
+            try
+            {
+                var service = new CustomerService();
+                Dictionary<string, string> user = new Dictionary<string, string>
+                {
+                    { "UserId", id },
+                };
+
+                StripeList<Customer> customers = service.List( user );
+
+                if (customers.size() > 0)
+                {
+                    Customer customer = customers.get(0);
+                    Customer customer = await service.GetAsync(customer.Metadata({ "UserId", id});
+                }
+                return customer;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<dynamic> PostCustomer(string id, string email, string firstName, string lastName)
+        {
+            try
+            {
+                StripeConfiguration.ApiKey = "sk_test_51Iyd0JCW5oBVi3aeyirlYffw09mn2TFbGyt10imL1VdyHYJq46wYgBs4fF6xMLhZBhGqkAwfQrJ9PpQ6qxT8XBZT000gUFLyzy";
+
+                var options = new CustomerCreateOptions
+                {
+                    Email = email,
+                    Name = firstName + " " + lastName,
+                    PaymentMethod = "pm_1FWS6ZClCIKljWvsVCvkdyWg",
+                    Metadata = new Dictionary<string, string>
+                    {
+                        { "UserId", id },
+                    }
+                };
+                var service = new CustomerService();
+                Customer customer = await service.CreateAsync(options);
+
+                return customer;
             }
             catch (Exception)
             {
